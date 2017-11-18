550d3c9d9cb6249808e5c47a019ddf01dd87f87c
albertmas <almaco_io@hotmail.es>
2017-11-18 01:57:10 +0100

    enemies now respawn along with player when you die

--- platformer/Motor2D/EntityPlayer.cpp
+++ platformer/Motor2D/EntityPlayer.cpp
@@ -223,7 +223,7 @@ bool EntityPlayer::PreUpdate(float dt)
 
 	if (set_to_start_pos == true)
 	{
-		SetPosOrigin();
+		App->map->change_to_this_level = App->map->index_map;
 		set_to_start_pos = false;
 	}
 	return true;
